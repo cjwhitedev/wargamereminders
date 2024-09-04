97d3e9c8db8992193be5b6c1c2edba8e7ac6ad6b
White, CJ <christopher.white2@pega.com>
2024-09-04 14:29:11 -0400

    add tailwind

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Deploy with `npm run deploy`
 
 ---
 
-# Getting Started with Create React App
+#npm install -D tailwindcss Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Wargame Reminders React App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App prose lg:prose-xl container mx-auto mt-4 bg-white p-8 rounded-lg shadow-md">
+      <h1 className="">Wargame Reminders</h1>
+      <p>
+        This project use React, Tailwind, and several of it's Official Plugins.
+      </p>
+
+      <div className="mt-8 max-w-md">
+        <div className="grid grid-cols-1 gap-6">
+          <label className="block">
+            <span className="text-gray-700">Full name</span>
+            <input
+              type="text"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              placeholder=""
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">Email address</span>
+            <input
+              type="email"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              placeholder="john@example.com"
+            />
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
