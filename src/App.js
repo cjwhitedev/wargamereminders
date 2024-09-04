33e11e92cd9b830be59d1b4779124d8f987ae55a
White, CJ <christopher.white2@pega.com>
2024-09-04 16:47:48 -0400

    started playing with data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import XMLData from "./data/wh40k-10e/Warhammer 40,000.gst";
 
 function App() {
-  return (
-    <div className="App prose lg:prose-xl container mx-auto mt-4 bg-white p-8 rounded-lg shadow-md">
-      <h1 className="">Wargame Reminders</h1>
-      <p>
-        This project use React, Tailwind, and several of it's Official Plugins.
-      </p>
+  const [name, setName] = useState("Wargame");
+  const [sharedRules, setSharedRules] = useState([]);
 
-      <div className="mt-8 max-w-md">
-        <div className="grid grid-cols-1 gap-6">
-          <label className="block">
-            <span className="text-gray-700">Full name</span>
-            <input
-              type="text"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder=""
-            />
-          </label>
-          <label className="block">
-            <span className="text-gray-700">Email address</span>
-            <input
-              type="email"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="john@example.com"
-            />
-          </label>
-        </div>
-      </div>
+  useEffect(() => {
+    axios
+      .get(XMLData, {
+        "Content-Type": "application/xml; charset=utf-8",
+      })
+      .then((response) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(response.data, "text/xml");
+
+        setName(
+          xmlDoc.getElementsByTagName("gameSystem")[0].getAttribute("name")
+        );
+
+        setSharedRules([]);
+        xmlDoc
+          .getElementsByTagName("sharedRules")[0]
+          .childNodes.forEach((rule) => {
+            if (rule.nodeName === "rule") {
+              setSharedRules((sharedRules) => [...sharedRules, rule]);
+            }
+          });
+      });
+  }, []);
+
+  return (
+    <div className="App container mx-auto mt-4 bg-white p-8 rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold mb-1">Wargame Reminders</h1>
+      <p className="mb-4">
+        You are seeing data from <span>{name}</span>. Here are some rules:
+      </p>
+      <ul className="list-none">
+        {sharedRules.map((rule, index) => (
+          <li className="mb-2" key={index}>
+            <span className="font-bold">{rule.getAttribute("name")}:</span>{" "}
+            {rule.firstElementChild.innerHTML}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
